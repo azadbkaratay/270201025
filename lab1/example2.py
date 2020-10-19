c31b20f632e634e7e5ba633be9c77256850a4680
b85460729 <jagazad@hotmail.com>
2020-10-19 10:34:57 +0000

    Finding Roots

--- lab1/example2.py
+++ lab1/example2.py
@@ -4,4 +4,4 @@ z=0.25
 up=((2*x+y)**2*z**0.5)
 down=(x**0.5)+(y**0.5)
 result=up/down
-print(result)
+print(result) 
---
+++ lab1/example3.py
@@ -0,0 +1,9 @@
+a=2
+b=6
+c=-20
+x1=((-b)+(b**2-4*a*c)**0.5)/(2*a)
+x2=((-b)-(b**2-4*a*c)**0.5)/(2*a)
+quadratic_equation= a*x1**2+b*x1+c
+quadratic_equation2=a*x2**2+b*x2+c
+print("First Root=",x1,"Second Root=",x2)
+
